f43ba1ae107429b05f684bbf3056c2d0d4cf91ee
Kleberton Santos <ti.klebersantos@gmail.com>
2021-09-12 16:45:54 -0300

    Projeto de API Filmes

---
+++ FilmesAPI/Data/Dtos/UpdateFilmeDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class UpdateFilmeDto
+    {
+        [Required(ErrorMessage = "O campo titulo e obrigatorio")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor e obrigatorio")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O genero nao pode passar de 30 caracteres")]
+        public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A duração deve ter no minimo 1 e no maximo 600 minutos")]
+        public int Duracao { get; set; }
+    }
+}
